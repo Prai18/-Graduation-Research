ccd5bea30ed352c0144ab4f6cf2bd9e7c16d19e0
Prai18 <daichi.sino@gmail.com>
2018-03-07 22:11:45 +0900

    左右にシフトできるようにしたンゴ

--- element-shift.c
+++ element-shift.c
@@ -1,33 +1,57 @@
 #include<stdio.h>
 #define N 5
-/*要素を１つ左にシフトさせる*/
+/*要素を１つ左右にシフトさせる*/
 
-void Array_Shift(int *);
+void Array(int *);
+void Array_Left_Shift(int *);
+void Array_Right_Shift(int *);
+
+int b[N];
 
 int main(int argc, char const *argv[])
 {
 	int a[N]={1,2,3,4,5};
-	int b[5];
+	
+	Array(a);
 
-	Array_Shift(a);
+	Array_Left_Shift(a);
 
-	for (int i = N-1; i >=0; --i)
-	{
-		if(i==0){
-			b[N-1]=a[i];
-		}else{
-			b[i-1]=a[i];
-		}
-	}
+	Array(b);
 
-	Array_Shift(b);
+	Array_Right_Shift(a);
+
+	Array(b);
 	return 0;
 }
 
-void Array_Shift(int *ar){
+void Array(int *ar){
 	for (int i = 0; i < 5; ++i)
 	{
 		printf("%d",ar[i]);
 	}
 		printf("\n");
+}
+
+
+void  Array_Left_Shift(int *ar){
+	
+	for (int i = N-1; i >=0; --i)
+	{
+		if(i==0){
+			b[N-1]=ar[i];
+		}else{
+			b[i-1]=ar[i];
+		}
+	}
+}
+
+void Array_Right_Shift(int *ar){
+	for (int i = 0; i < N; ++i)
+	{
+		if(i==N-1){
+			b[0]=ar[i];
+		}else{
+			b[i+1]=ar[i];
+		}
+	}
 }
