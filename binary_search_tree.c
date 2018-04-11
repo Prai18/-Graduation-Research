a4727eea6e6392d7f884c3e119b71555e294801e
Prai18 <daichi.sino@gmail.com>
2018-04-11 17:32:29 +0900

    二分探索(院試問題)

---
+++ binary_search_tree.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/*二分探索木(熊大院試問題)*/
+
+#define N 9
+
+typedef struct node{
+    int key;
+    struct node *left;
+    struct node *right;
+}Node;
+
+Node *create(int key){
+    Node *x;
+    x=malloc(sizeof(Node));
+    x->key=key;
+    x->left=NULL;
+    x->right=NULL;
+    return x;
+}
+
+Node *tree_search(Node *x,int k){
+    if(x==NULL)return NULL;
+    if(/* (ア) */)return x;
+    if(/* (イ) */){
+    return tree_search(x->left,k);
+}else{
+    return tree_search(x->right,k);
+}
+
+void tree_insert(Node *x,int key){
+    if(x->key<=key){
+        if(x->right==NULL){
+            x->right=create_node(key);
+        }else{
+            tree_insert(/* (ウ) */,key);
+        }
+    }else{
+        if(x->left==NULL){
+            x->left=create_node(key);
+        }else{
+            tree_insert(/* (エ) */,key);
+        }
+    }
+}
+
+int main(){
+    Node *root,*x;
+    int array[N]={9,16,8,18,14,3,15,12};
+    int i,search_key=12;
+    root=create_node(array[0]);
+    for(i=1;i<N;i++){
+        tree_insert(root,array[i]);
+    }
+    x=tree_search(root,search_key);
+}
