ebe4e9df47dc9fdcc4cb4a935ce5a58c3bbe259b
Prai18 <daichi.sino@gmail.com>
2018-09-26 18:55:23 +0900

    2冪剰余環2変数可換多項式の計算(行列)

--- 2変数可換多項式の計算(行列).c
+++ 2変数可換多項式の計算(行列).c
@@ -2,23 +2,34 @@
 #include<stdlib.h>
 
 #define X 1											
-#define Y 1											
+#define Y -1											
 
-#define M_value 3											//m乗の値(正確にはm-1乗)
+#define xX 2											
+#define yY 1
 
-int m_matrix1[2][2]={{X,-Y},
+#define nN 1
+#define N 2											//n乗の値(正確にはm-1乗)
+
+int matrixA[2][2]={{X,Y},
 				 	{1,0}};	
 
-int m_matrix2[2][2]={{X,-Y},
-				 	{1,0}};				 									
+int matrixB[2][2]={{X,Y},
+				 	{1,0}};
 
-void exp_m(int m){
-	for (int i = 1; i < m-1; ++i)
+int matrixC[2][2];				 									
+
+void exp_n(int n){
+	for (int i = 1; i < n; ++i)
 	{
-		m_matrix1[0][0]=m_matrix1[0][0]*m_matrix2[0][0]+m_matrix1[0][1]*m_matrix2[1][0];
-		m_matrix1[0][1]=m_matrix1[0][0]*m_matrix2[0][1]+m_matrix1[0][1]*m_matrix2[1][1];
-		m_matrix1[1][0]=m_matrix1[1][0]*m_matrix2[0][0]+m_matrix1[1][1]*m_matrix2[1][0];
-		m_matrix1[1][1]=m_matrix1[1][0]*m_matrix2[0][1]+m_matrix1[1][1]*m_matrix2[1][1];
+		matrixC[0][0]=matrixA[0][0]*matrixB[0][0]+matrixA[0][1]*matrixB[1][0];
+		matrixC[0][1]=matrixA[0][0]*matrixB[0][1]+matrixA[0][1]*matrixB[1][1];
+		matrixC[1][0]=matrixA[1][0]*matrixB[0][0]+matrixA[1][1]*matrixB[1][0];
+		matrixC[1][1]=matrixA[1][0]*matrixB[0][1]+matrixA[1][1]*matrixB[1][1];
+
+		matrixA[0][0]=matrixC[0][0];
+		matrixA[0][1]=matrixC[0][1];
+		matrixA[1][0]=matrixC[1][0];
+		matrixA[1][1]=matrixC[1][1];
 	}
 }	
 
@@ -36,7 +47,7 @@ void print_matrix21(int matrix[2][1]){				//2×1の行列の表示
 }
 
 void print_matrix22(int matrix[2][2]){				//2×2の行列の表示
-	printf("******m乗の行列*******\n");
+	printf("******%d乗の行列*******\n",N);
 	for (int i = 0; i < 2; ++i)
 	{
 		for (int j = 0; j < 2; ++j)
@@ -52,17 +63,31 @@ int main(int argc, char const *argv[])
 	int twone[2][1]={{X},
 					 {2}};
 
-	int ans_matrix[2][1];							//2変数可換多項式のx成分
+	int f_matrix[2][1];							//2変数可換多項式のx成分
 
-	exp_m(M_value);										/*|X -Y|
-													  	  |1  0|のm-1乗の計算をする*/
+	int T2,T1;
+	int ans;
+	// for (int i = 0; i < 5; ++i)
+	// {
+	// 	printf("%d乗します。\n",i+2);
+	// 	exp_m(N);
+	// 	print_matrix22(matrixA);
+	// }
+	
+	exp_n(N);											/*|X  Y|
+													  	  |1  0|のn乗の計算をする*/
 	
 	
-	print_matrix22(m_matrix1);
+	print_matrix22(matrixA);
 
-	ans_matrix[0][0]=m_matrix1[0][0]*twone[0][0]+m_matrix1[0][1]*twone[1][0];					//これ以降は無視していいよ
-	ans_matrix[1][0]=m_matrix1[1][0]*twone[0][0]+m_matrix1[1][1]*twone[1][0];
-	print_matrix21(ans_matrix);
+	f_matrix[0][0]=matrixC[0][0]*twone[0][0]+matrixC[0][1]*twone[1][0];					//これ以降は無視していいよ
+	f_matrix[1][0]=matrixC[1][0]*twone[0][0]+matrixC[1][1]*twone[1][0];
+	print_matrix21(f_matrix);
 
+	T2=f_matrix[0][0];
+	T1=f_matrix[1][0];
+
+	ans=(xX*T2-yY*T1)%nN;
+	printf("%d\n",nN);
 	return 0;
 }
